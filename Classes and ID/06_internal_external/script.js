a201139e4aeb798af6c0d0929d69c8c095b02916
GunaPalanivel <gunapalanivel2003@gmail.com>
2023-12-17 10:03:57 +0530

    Added Explanations

--- Classes and ID/06_internal_external/script.js
+++ Classes and ID/06_internal_external/script.js
@@ -1,9 +1,11 @@
-function message()
-{
-    alert("BETTER TO TRY SOMEWHERE!")
+function message() {
+  // Defines a function named "message"
+  alert("BETTER TO TRY SOMEWHERE!"); // Displays an alert with the message "BETTER TO TRY SOMEWHERE!"
 }
 
-function internal()
-{
-    alert("Order is in progress waiting for the information from the account teams")
+function internal() {
+  // Defines a function named "internal"
+  alert(
+    "Order is in progress waiting for the information from the account teams"
+  ); // Displays an alert with the message "Order is in progress waiting for the information from the account teams"
 }
